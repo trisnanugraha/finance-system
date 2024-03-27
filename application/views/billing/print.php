8b301e0e00b5547cf54568712660bf9e7e5d2765
Trisna Nugraha <trisna.nugraha123@gmail.com>
2024-03-27 14:45:00 +0700

    Resize VA BCA Code on Billing

--- application/views/billing/print.php
+++ application/views/billing/print.php
@@ -252,10 +252,10 @@
                         </tr>
                         <tr>
                             <td class="border padding-left padding-right" colspan="2" style="padding-top: 5px; padding-bottom:5px;">
-                                <p style="font-size: 15px;">Transfer to Account : PERHIMPUNAN PENGHUNI SCBD SUITES at ARTHA GRAHA A/C: 008-1-28286-2</p>
+                                <p>Transfer to Account : PERHIMPUNAN PENGHUNI SCBD SUITES at ARTHA GRAHA A/C: 008-1-28286-2</p>
                                  Please pay to your BCA Virtual Account with Beneficiary Name : <span id="virtual-account-name"><?= strtoupper($billing->nama_customer); ?></span>
                                 <ol type="1">
-                                    <li>Through Automatic Teller Machine (ATM) of Bank Central Asia (BCA) by Choosing transfer menu to your BCA Virtual Account for <span id="virtual-account-number"><?= $billing->kode_virtual; ?></span></li>
+                                    <li>Through Automatic Teller Machine (ATM) of Bank Central Asia (BCA) by Choosing transfer menu to your <span style="font-size: 15px;">BCA Virtual Account for <span id="virtual-account-number"><?= $billing->kode_virtual; ?></span></span></li>
                                     <li>Through transfer by cashform Bank Central Asia (BCA) to your BCA Virtual Account for <span id="virtual-account-number"><?= $billing->kode_virtual; ?></span></li>
                                     <li>Transfer to Account : PERHIMPUNAN PENGHUNI SCBD SUITES at ARTHA GRAHA A/C: 008-1-28286-2</li>
                                 </ol>
